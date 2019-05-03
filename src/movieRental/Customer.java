98cc11ee0037bc0a6346ce6d64f8b24a6b44d200
Joshua Schulz <joshua.schulz@sap.com>
2019-05-03 09:02:43 +0200

    refactored names for parameters

--- src/movieRental/Customer.java
+++ src/movieRental/Customer.java
@@ -13,8 +13,8 @@ class Customer {
 
     ;
 
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    public void addRental(Rental newRental) {
+        rentals.addElement(newRental);
     }
 
     ;
@@ -52,21 +52,21 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental rental) {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDREN:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
             default:
                 //in case of an error
